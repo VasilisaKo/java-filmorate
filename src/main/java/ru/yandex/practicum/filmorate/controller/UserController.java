6b2b7821398c8bb91efe13a8cb30ad7f0decdede
Vasilisa <vasjatinka@yandex.ru>
2023-03-09 14:37:46 +0000

    delete: SimpleContoller fix: add log.error in UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -26,15 +26,18 @@ public class UserController {
     @PostMapping
     public User create(@RequestBody User user) {
         if(user.getEmail() == null || !user.getEmail().contains("@")) {
+            log.error("Электронная почта не может быть пустой и должна содержать символ @.");
             throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @.");
         }
         if (user.getLogin() == null || user.getLogin().contains(" ")){
+            log.error("Логин не может быть пустым и содержать пробелы.");
             throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
         }
         if (user.getName() == null || user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.error("Дата рождения не может быть в будущем.");
             throw new ValidationException("Дата рождения не может быть в будущем.");
         }
         user.setId(idGenerator++);
